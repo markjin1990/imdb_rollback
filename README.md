dcf592085760b5f277cee696142dd806d1722a15
Zhongjun Jin <markjin1990@gmail.com>
2017-04-21 17:04:23 -0400

    resolved the performance issue in the previous commit; added a README file

---
+++ README.md
@@ -0,0 +1,11 @@
+# IMDB Rollback
+
+IMDB Rollback is a bash script that helps to download imdb data and roll it back to a previous date.
+
+### Execution
+
+To rollback imdb data to date YYMMDD:
+
+```sh
+$ ./imdb_rollback.sh -d YYMMDD
+```
--- imdb_rollback.sh
+++ imdb_rollback.sh
@@ -7,9 +7,10 @@
 
 imdbAttr='ftp://ftp.fu-berlin.de/pub/misc/movies/database/'
 imdbDiffAttr='ftp://ftp.fu-berlin.de/pub/misc/movies/database/diffs/'
+diffDir='./diffs'
 
 echo "====================="
-echo "IMDB DATA FOR TESTING"
+echo "Rollback IMDB data to a previous date"
 echo ""
 echo "Author: Zhongjun Jin (markjin1990@gmail.com)"
 echo "====================="
@@ -18,25 +19,31 @@ echo "[STEP 0] Cleaning local data"
 # rm *.gz
 
 echo "[STEP 1] Downloading IMDB data"
-# wget -q -r --accept="*.gz" --no-directories --no-host-directories --level 1 ftp://ftp.fu-berlin.de/pub/misc/movies/database/
+# wget -r --accept="*.gz" --no-directories --no-host-directories --level 1 ftp://ftp.fu-berlin.de/pub/misc/movies/database/   | sed 's/^/       /'
 
 
 if [ "$1" = "-d" ] && [ ${#2} = 6 ]; then
 
-    # echo "[STEP 2] Downloading ApplyDiffs-2.5.tar.gz"
-    # mkdir diffs
-    # wget ftp://ftp.fu-berlin.de/pub/misc/movies/database/diffs/ApplyDiffs-2.5.tar.gz -O ApplyDiffs-2.5.tar.gz
-    # gzip -f -d ApplyDiffs-2.5.tar.gz
-    # tar -xf ApplyDiffs-2.5.tar -C .
-    # make -C ApplyDiffs/
+    echo "[STEP 2] Decompressing IMDB data"
 
-    echo "[STEP 2] Downloading diff files older than $2 and rollback"
+    tarFiles=($(ls *.gz))
+    for tarFile in "${tarFiles[@]}"
+    do
+        echo "    => decompress $tarFile"
+        gzip -f -d "$tarFile" | sed 's/^/       /'
+    done
+
+    echo "[STEP 3] Downloading diff files older than $2"
+
+    # Create a ./diffs directory
+    mkdir -p "$diffDir"
 
     # get all files in diff directory
     echo "    => Get the list of diff files from $imdbDiffAttr"
     tempFilenames=($(curl -l $imdbDiffAttr))
     diffFileName=($(for l in ${tempFilenames[@]}; do echo $l; done | sort))
 
+    echo "[STEP 4] Rollback"
     if [ "$2" -lt "990000" ]
     then
         digit1=${2:0:1}
@@ -60,6 +67,9 @@ if [ "$1" = "-d" ] && [ ${#2} = 6 ]; then
         # Regex to check the files older than user-specified date
         regex="diffs-([$digit1plus1-8][0-9]{5}|$digit1[$digit2plus1-9][0-9]{4}|$digit1$digit2[$digit3plus1-9][0-9]{3}|$digit1$digit2$digit3[$digit4plus1-9][0-9]{2}|$digit1$digit2$digit3$digit4[$digit5plus1-9][0-9]|$digit1$digit2$digit3$digit4$digit5[$digit6-9]).tar.gz"
 
+
+
+
         # Check if the file name matches the regex. If it does, download the file to diff directory. The file is sorted in a reversed order.
         for (( idx=${#diffFileName[@]}-1 ; idx>=0 ; idx-- )) ; 
         do
@@ -67,27 +77,20 @@ if [ "$1" = "-d" ] && [ ${#2} = 6 ]; then
             if [[ ${diffFileName[idx]} =~ ^$regex$ ]];
             then
                 echo "    => Download diff file ${diffFileName[idx]}"
-                wget -q "$imdbDiffAttr${diffFileName[idx]}" -O ./diffs/${diffFileName[idx]}  | sed 's/^/       /'
+                wget -q "$imdbDiffAttr${diffFileName[idx]}" -O "$diffDir/${diffFileName[idx]}"  | sed 's/^/       /'
                 echo "    => Decompress ${diffFileName[idx]}"
-                gzip -f -d diffs/${diffFileName[idx]}  | sed 's/^/       /'
-                tar -xf diffs/${diffFileName[idx]:0:${#diffFileName[idx]}-3}  | sed 's/^/       /'
+                gzip -f -d "$diffDir/${diffFileName[idx]}"  | sed 's/^/       /'
+                tar -xf "$diffDir/${diffFileName[idx]:0:${#diffFileName[idx]}-3}"  | sed 's/^/       /'
 
                 echo "    => Roll back data using ${diffFileName[idx]}"
-                diffFiles=($(ls ./diffs/*.list))
+                diffFiles=($(ls ./$diffDir/*.list))
                 for diffFile in "${diffFiles[@]}"
                 do
                     
                     curFile=$(basename "$diffFile")
 
-                    gzip -f -d "$curFile.gz"  | sed 's/^/       /'
-
                     patch -R $curFile $diffFile  | sed 's/^/       /'
 
-                    # output=$( patch -R $curFile $diffFile )
-
-                    # echo "patch output $output"
-
-                    gzip -f "$curFile" | sed 's/^/       /'
                 done
                 echo ""
             fi
@@ -117,34 +120,28 @@ if [ "$1" = "-d" ] && [ ${#2} = 6 ]; then
 
 
         # files older than user-specified date but earlier than year 2000
-        regex2="([$digit1plus1-9][0-9]{5}|$digit1[$digit2plus1-9][0-9]{4}|$digit1$digit2[$digit3plus1-9][0-9]{3}|$digit1$digit2$digit3[$digit4plus1-9][0-9]{2}|$digit1$digit2$digit3$digit4[$digit5plus1-9][0-9]|$digit1$digit2$digit3$digit4$digit5[$digit6-9])"
+        regex2="diffs-([$digit1plus1-9][0-9]{5}|$digit1[$digit2plus1-9][0-9]{4}|$digit1$digit2[$digit3plus1-9][0-9]{3}|$digit1$digit2$digit3[$digit4plus1-9][0-9]{2}|$digit1$digit2$digit3$digit4[$digit5plus1-9][0-9]|$digit1$digit2$digit3$digit4$digit5[$digit6-9])"
 
         # Check if the file name matches the regex. If it does, download the file to diff directory.
         for (( idx=${#diffFileName[@]}-1 ; idx>=0 ; idx-- )) ; 
         do
-            if [ ${diffFileName[idx]} =~ ^$regex$ ]
+            if [ ${diffFileName[idx]} =~ ^$regex1$ ]
             then
                 echo "    => Download diff file ${diffFileName[idx]}"
-                wget -q "$imdbDiffAttr${diffFileName[idx]}" -O ./diffs/${diffFileName[idx]}  | sed 's/^/       /'
+                wget -q "$imdbDiffAttr${diffFileName[idx]}" -O "$diffDir/${diffFileName[idx]}"  | sed 's/^/       /'
                 echo "    => Decompress ${diffFileName[idx]}"
-                gzip -f -d diffs/${diffFileName[idx]}  | sed 's/^/       /'
-                tar -xf diffs/${diffFileName[idx]:0:${#diffFileName[idx]}-3}  | sed 's/^/       /'
+                gzip -f -d "$diffDir/${diffFileName[idx]}"  | sed 's/^/       /'
+                tar -xf "$diffDir/${diffFileName[idx]:0:${#diffFileName[idx]}-3}"  | sed 's/^/       /'
 
                 echo "    => Roll back data using ${diffFileName[idx]}"
-                diffFiles=($(ls ./diffs/*.list))
+                diffFiles=($(ls "$diffDir/*.list"))
                 for diffFile in "${diffFiles[@]}"
                 do
                     curFile=$(basename "$diffFile")
 
-                    gzip -f -d "$curFile.gz" | sed 's/^/       /'
 
                     patch -R $curFile $diffFile | sed 's/^/       /'
 
-                    # output=$( patch -R $curFile $diffFile )
-
-                    # echo "patch output $output"
-
-                    gzip -f "$curFile" | sed 's/^/       /'
                 done
 
                 echo ""
@@ -153,33 +150,34 @@ if [ "$1" = "-d" ] && [ ${#2} = 6 ]; then
 
         for (( idx=${#diffFileName[@]}-1 ; idx>=0 ; idx-- )) ; 
         do
-            if [ ${diffFileName[idx]} =~ ^$regex$ ]
+            if [ ${diffFileName[idx]} =~ ^$regex2$ ]
             then
                 echo "    => Download diff file ${diffFileName[idx]}"
-                wget -q "$imdbDiffAttr{diffFileName[idx]}" -O ./diffs/${diffFileName[idx]}  | sed 's/^/       /'
+                wget -q "$imdbDiffAttr{diffFileName[idx]}" -O "$diffDir/${diffFileName[idx]}"  | sed 's/^/       /'
                 echo "    => Decompress ${diffFileName[idx]}"
-                gzip -f -d diffs/${diffFileName[idx]}  | sed 's/^/       /'
-                tar -xf diffs/${diffFileName[idx]:0:${#diffFileName[idx]}-3}  | sed 's/^/       /'
+                gzip -f -d "$diffDir/${diffFileName[idx]}"  | sed 's/^/       /'
+                tar -xf "$diffDir/${diffFileName[idx]:0:${#diffFileName[idx]}-3}"  | sed 's/^/       /'
 
                 echo "    => Roll back data using ${diffFileName[idx]}"
-                diffFiles=($(ls ./diffs/*.list))
+                diffFiles=($(ls "$diffDir/*.list"))
                 for diffFile in "${diffFiles[@]}"
                 do
                     curFile=$(basename "$diffFile")
 
-                    gzip -f -d "$curFile.gz" | sed 's/^/       /'
-
                     patch -R $curFile $diffFile | sed 's/^/       /'
 
-                    # output=$( patch -R $curFile $diffFile )
-
-                    # echo "patch output $output"
-
-                    gzip -f "$curFile" | sed 's/^/       /'
                 done
                 echo ""
             fi
         done
     fi
+
+    echo "[STEP 5] Compressing IMDB data"
+    tarFiles=($(ls *.list))
+    for tarFile in "${tarFiles[@]}"
+    do
+        echo "      => compress $tarFile"
+        gzip -f "$tarFile" | sed 's/^/       /'
+    done
     
 fi
